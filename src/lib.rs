56b87e9d4733639f4ab255602e912c3c757f7a26
Valentin Semenov <valentins@MacBook-Pro-Valentin.local>
2024-12-25 17:12:36 +0300

    feat: add tests for func zip_dir

--- src/lib.rs
+++ src/lib.rs
@@ -38,8 +38,11 @@ pub fn zip_directory(path_to_report_dir: &str) -> Result<PathBuf, Box<dyn Error>
     let options = SimpleFileOptions::default()
         .compression_method(CompressionMethod::Deflated)
         .unix_permissions(0o755);
-
-    for entry in read_dir(path_to_report_dir)? {
+    let read_directory = match read_dir(path_to_report_dir) {
+        Ok(value) => value,
+        _ => return Err(format!("Не нашли директорию по пути: <{:?}>", path_to_report_dir).into()),
+    };
+    for entry in read_directory {
         match entry {
             Ok(value) => {
                 // Читаем содержимое исходного файла
@@ -50,7 +53,7 @@ pub fn zip_directory(path_to_report_dir: &str) -> Result<PathBuf, Box<dyn Error>
                     // Добавляем файл в ZIP-архив
                     let file_name_archive = match value.file_name().to_str() {
                         Some(file_name) => file_name.to_string(),
-                        None => return Err("Какая-то херня".into()),
+                        None => return Err("Не смогли привести имя файла к строке".into()),
                     };
                     zip.start_file(format!("{}", file_name_archive).to_string(), options)?;
                     zip.write_all(&buffer)?;
@@ -86,5 +89,60 @@ mod tests {
                 archive_file_name
             );
         }
+    }
+
+    #[test]
+    /// Проверяем архивацию пустой папки
+    fn test_zip_empty_directory() {
+        // Получаем полный путь до папки для архивирования и создаем ее(пустая без файлов)
+        let binding = UserDirs::new().unwrap();
+        let desktop_dir = binding.desktop_dir().unwrap();
+        let full_path: PathBuf = desktop_dir.join("dir_for_test_zip_directory");
+        let _ = fs::create_dir_all(&full_path);
+        // Используем функцию zip_directory и проверяем полученный путь
+        let zip_dir = zip_directory(&full_path.to_str().unwrap()).unwrap_or_default();
+        let re = Regex::new(r"testops_results_report_\d+\.zip").unwrap();
+        if !re.is_match(&zip_dir.to_str().unwrap_or_default()) {
+            assert!(
+                false,
+                "Получили dir_path == <{:?}>. Ожидали что имя файла будет соответствовать шаблону testops_results_report_\\d+\\.zip",
+                &zip_dir.to_str().unwrap_or_default()
+            );
+        }
+    }
+
+    #[test]
+    /// Проверяем архивацию директории, в которой есть файл
+    fn test_zip_dir_with_one_file() {
+        // Получаем полный путь до папки для архивирования и создаем ее(пустая без файлов)
+        let binding = UserDirs::new().unwrap();
+        let desktop_dir = binding.desktop_dir().unwrap();
+        let full_path: PathBuf = desktop_dir.join("dir_for_test_zip_directory_with_one_file");
+        let _ = fs::create_dir_all(&full_path);
+        // Создаем файл в директории
+        let _ = File::create(full_path.join("some_file.json")).unwrap();
+        // Используем функцию zip_directory и проверяем полученный путь
+        let zip_dir = zip_directory(&full_path.to_str().unwrap()).unwrap_or_default();
+        let re = Regex::new(r"testops_results_report_\d+\.zip").unwrap();
+        if !re.is_match(&zip_dir.to_str().unwrap_or_default()) {
+            assert!(
+                false,
+                "Получили dir_path == <{:?}>. Ожидали что имя файла будет соответствовать шаблону testops_results_report_\\d+\\.zip",
+                &zip_dir.to_str().unwrap_or_default()
+            );
+        }
+    }
+
+    #[test]
+    /// Проверяем архивацию несуществующей директории
+    fn test_nonexistent() {
+        let expected_error =
+            "Не нашли директорию по пути: <\"nonexistent_dir_for_test\">".to_string();
+        let actual_error = zip_directory("nonexistent_dir_for_test");
+        assert_eq!(
+            expected_error,
+            actual_error.unwrap_err().to_string(),
+            "Ошибка, когда передаем несуществующую директорию"
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use wot::zip_directory;
+// https://github.com/clap-rs/clap?tab=readme-ov-file
+// https://docs.rs/clap/latest/clap/
 
 fn main() {
     let result = zip_directory("/Users/valentins/Desktop/test_allure_report");
