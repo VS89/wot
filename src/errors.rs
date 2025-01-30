ba45a40978e334c39780c247e83515080e829219
Valentin Semenov <valentins@MacBook-Pro-Valentin.local>
2025-01-30 18:32:30 +0300

    rename errors const to en

--- src/errors.rs
+++ src/errors.rs
@@ -25,42 +25,48 @@ pub enum WotError {
 impl fmt::Display for WotError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            WotError::NotFoundUserDir => write!(f, "Не удалось получить каталоги пользователя"),
-            WotError::InvalidURL => write!(f, "Введенная строка должна быть URL"),
+            WotError::NotFoundUserDir => write!(f, "Failed to retrieve the user's directories"),
+            WotError::InvalidURL => write!(f, "The string entered must be a URL"),
             WotError::NotReadFileByPath(file_path) => {
-                write!(f, "Получили ошибку при чтение файла по пути: {file_path}")
+                write!(
+                    f,
+                    "Received an error when reading a file at path: {file_path}"
+                )
             }
             WotError::InvalidToken => {
-                write!(f, "Ваш токен не прошел валидацию, попробуйте еще раз")
+                write!(f, "Your token failed validation, please try again")
             }
             WotError::ProjectIdNotFound(project_id) => {
                 write!(f, "Project with ID == {project_id} not found")
             }
-            WotError::NotParseConfig => write!(f, "Не смогли распарсить конфиг"),
+            WotError::NotParseConfig => write!(f, "Couldn't parse the config"),
             WotError::NotFoundDirByPath(file_path) => {
-                write!(f, "Не нашли директорию по пути: <{file_path}>")
+                write!(f, "Could not find the directory at path: <{file_path}>")
             }
-            WotError::ParseFileNameToStr => write!(f, "Не смогли привести имя файла к строке"),
-            WotError::ParseHeaderValue => write!(f, "Не смогли преобразовать HeaderValue"),
+            WotError::ParseFileNameToStr => write!(f, "Could not cast the filename to a string"),
+            WotError::ParseHeaderValue => write!(f, "Could not convert HeaderValue"),
             WotError::NotReadFile(path, error) => write!(
                 f,
-                "Не смогли прочитать файл по пути: \"{path}\". Получили ошибку: {error}",
+                "Couldn't read the file in the path: \"{path}\". We got an error: {error}",
             ),
             WotError::ExtensionZip(extension_file) => write!(
                 f,
-                "Нужен файл с расширением .zip, был передан файл: *.{extension_file}"
+                "Need a file with a .zip extension, a file was transferred:*.{extension_file}"
             ),
             WotError::NotFileName(file_path) => {
-                write!(f, "Не удалось получить имя файла из пути: \"{file_path}\"")
+                write!(
+                    f,
+                    "Failed to get the file name from the path:\"{file_path}\""
+                )
             }
             WotError::ProjectIdMoreThenZero => {
-                write!(f, "project_id должен быть целым числом > 0")
+                write!(f, "project_id must be an integer > 0")
             }
             WotError::CouldReadLine => {
                 write!(f, "Couldn't read the line")
             }
             WotError::CantCreateConfig => {
-                write!(f, "Не смогли создать конфиг")
+                write!(f, "Couldn't create a config")
             }
         }
     }
@@ -80,15 +86,15 @@ impl fmt::Display for WotApiError {
         match self {
             WotApiError::Multipart(file_name, error) => write!(
                 f,
-                "Не смогли преобразовать файл {file_name} для API-запроса. Ошибка: {error}"
+                "Could not convert file {file_name} for API request. Error: {error}"
             ),
             WotApiError::EmptyResponse(method) => {
-                write!(f, "Получили пустой ответ от метода {method}")
+                write!(f, "Received an empty response from method {method}")
             }
             WotApiError::ParsingResponse(method, error) => {
                 write!(
                     f,
-                    "При парсинге ответа метода {method} получили ошибку: {error}"
+                    "When parsing the response of method {method}, we got an error: {error}"
                 )
             }
         }
