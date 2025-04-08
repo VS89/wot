0928495792b3a7545f02a041b0d2da3c6162e916
Valentin Semenov <spbvalek@gmail.com>
2025-04-08 17:46:11 +0300

    fix: format link report (#10)
    Co-authored-by: Valentin Semenov <valentins@MacBook-Pro-Valentin.local>

--- src/constants.rs
+++ src/constants.rs
@@ -16,7 +16,7 @@ impl Message {
         match self {
             Message::LaunchRunFrom(name) => format!("Run from {}", name),
             Message::LaunchLinkDownloaded(testops_instance, launch_id) => format!(
-                "Link to downloaded lunch: {}/launch/{}",
+                "Link to downloaded lunch: {}launch/{}",
                 testops_instance, launch_id
             ),
             Message::ApproveUploadReport(value) => format!(
@@ -24,5 +24,49 @@ impl Message {
                 value
             ),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::TestopsApi;
+    use rstest::rstest;
+
+    fn get_testops_instance_url() -> String {
+        TestopsApi::default().client.base_url.to_string()
+    }
+
+    #[rstest]
+    #[case("SomeName", "Run from SomeName")]
+    #[case("", "Run from ")]
+    #[case("Some&Name", "Run from Some&Name")]
+    #[case("a".repeat(1000), format!("Run from {}", "a".repeat(1000)))]
+    fn test_launch_run_from(#[case] input: String, #[case] expected: String) {
+        assert_eq!(Message::LaunchRunFrom(input).to_formatted_string(), expected);
+    }
+
+    #[rstest]
+    #[case(get_testops_instance_url(), "12345", format!("Link to downloaded lunch: {}launch/12345", get_testops_instance_url()))]
+    #[case("", "", "Link to downloaded lunch: launch/")]
+    #[case("a".repeat(1000), "a".repeat(1000), format!("Link to downloaded lunch: {}launch/{}", "a".repeat(1000), "a".repeat(1000)))]
+    fn test_launch_link_downloaded(#[case] instance: String, #[case] launch_id: String, #[case] expected: String) {
+        assert_eq!(
+            Message::LaunchLinkDownloaded(instance, launch_id).to_formatted_string(),
+            expected
+        );
+    }
+
+    #[rstest]
+    #[case("TestProject", "You want to load a report into a project: 'TestProject' [y/n]? ")]
+    #[case("", "You want to load a report into a project: '' [y/n]? ")]
+    #[case("Test&Project", "You want to load a report into a project: 'Test&Project' [y/n]? ")]
+    #[case("a".repeat(1000), format!("You want to load a report into a project: '{}' [y/n]? ", "a".repeat(1000)))]
+    fn test_approve_upload_report(#[case] input: String, #[case] expected: String) {
+        assert_eq!(
+            Message::ApproveUploadReport(input).to_formatted_string(),
+            expected
+        );
     }
 }
